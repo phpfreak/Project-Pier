ccc96b639a0ba2e898b2fdca9a443cf4d2840963
phpfreak <reinier.van.loon@gmail.com>
2011-12-03 13:35:49 +0100

    commit 95 removed old themes, old javascript (yui), minimized image size

--- public/assets/javascript/modules/addProjectForm.js
+++
@@ -1,6 +0,0 @@
-App.modules.addProjectForm = {
-  formActionClick: function() {
-    $('projectFormActionSelectMessage').disabled = !$('projectFormActionAddComment').checked;
-    $('projectFormActionSelectTaskList').disabled = !$('projectFormActionAddTask').checked;
-  }
-};
