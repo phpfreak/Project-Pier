3faefcdbf123a6c0d0ff41afd257a6d68c7cb8a8
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-02 00:15:00 +0800

    commit 113 in-page filter for tags

--- application/views/project/tags.php
+++ application/views/project/tags.php
@@ -6,10 +6,11 @@
 
 ?>
 <?php if (isset($tag_names) && is_array($tag_names) && count($tag_names)) { ?>
+<form action=""><input type=text id="filter"> <span id="filter-count"></span></form>
 <p><?php echo lang('tags used on projects') ?>:</p>
-<ul>
+<ul class="filtered">
 <?php foreach ($tag_names as $tag_name) { ?>
-  <li><a href="<?php echo active_project()->getTagUrl($tag_name) ?>"><?php echo clean($tag_name) ?></a> <span class="desc"> - <?php echo lang('number of tagged objects', active_project()->countObjectsByTag($tag_name)) ?></span></li>
+  <li><a href="<?php echo active_project()->getTagUrl($tag_name) ?>"><?php echo clean($tag_name) ?></a> <span class="desc">- <?php echo lang('number of tagged objects', active_project()->countObjectsByTag($tag_name)) ?></span></li>
 <?php } // foreach ?>
 </ul>
 <?php } else { ?>
