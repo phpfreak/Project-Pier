20954d555e54d9b0b1e1149f97942a83caf302c8
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-09 23:15:00 +0100

    commit 154 respect language selection and auto-insert missing items

--- environment/classes/localization/localization.php
+++ environment/classes/localization/localization.php
@@ -8,7 +8,7 @@
   * @return string
   */
   function lang($name) {
-    
+
     // Get function arguments and remove first one. 
     $args = func_get_args();
     if (is_array($args)) {
@@ -45,15 +45,34 @@
   * @return string
   */
   function lang_from_db($name) {
+    global $language, $locale_id;
     static $langs;
     if (!is_array($langs)) $langs = array();
     if (!array_key_exists($name, $langs)) {
+      if (!isset($language)) $language = 'en_us';
+      if (!isset($locale_id)) {
+        $language_country = explode('_', $language);
+        $a = $language_country[0];
+        $b = $language_country[1];
+        $sql = "select `id` from `".TABLE_PREFIX."i18n_locales` where language_code = '$a' and country_code = '$b'";
+        try {
+          $result = DB::executeOne($sql);
+          if ($result) {
+            $locale_id = $result['id'];
+          }
+        } catch(Exception $e) { $locale_id = 1; }
+      }
       $langs[$name] = $name;
-      $sql = "select `description` from `".TABLE_PREFIX."i18n_values` where `name` = '$name'";
+      $sql = "select `description` from `".TABLE_PREFIX."i18n_values` where `name` = '$name' and locale_id = '$locale_id'";
       try {
         $result = DB::executeOne($sql);
         if ($result) {
           $langs[$name] = $result['description'];
+        } else {
+          $sql = "insert into `".TABLE_PREFIX."i18n_values` (`locale_id`, `category_id`, `name`, `description`) values( '$locale_id', '$category_id', '$name', '~{$name}');";
+          try {
+            mysql_query($sql);
+          } catch(Exception $e) {}
         }
       } catch(Exception $e) {}
     }
