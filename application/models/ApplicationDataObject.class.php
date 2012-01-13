b78caf614608696a7ed3b268160c52ac26d88c1a
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-13 22:15:03 +0100

    commit 170 avoid unnecessary database access

--- application/models/ApplicationDataObject.class.php
+++ application/models/ApplicationDataObject.class.php
@@ -97,7 +97,9 @@
       trace(__FILE__,'getCreatedBy()');
       if (is_null($this->created_by)) {
         if ($this->columnExists('created_by_id')) {
-          $this->created_by = Users::findById($this->getCreatedById());
+          if ($this->getCreatedById()>0) {
+            $this->created_by = Users::findById($this->getCreatedById());
+          }
         }
       } // 
       return $this->created_by;
@@ -140,7 +142,9 @@
     function getUpdatedBy() {
       if (is_null($this->updated_by)) {
         if ($this->columnExists('updated_by_id')) {
-          $this->updated_by = Users::findById($this->getUpdatedById());
+          if ($this->getUpdatedById()>0) {
+            $this->updated_by = Users::findById($this->getUpdatedById());
+          }
         }
       } // 
       return $this->updated_by;
