baad365de2d0c42a9b09dc4b99cdb27170852eb6
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-21 22:14:53 +0100

    commit 185 display image files as images in project description and auto-resize

--- application/views/page_attachment/view_ProjectFiles.php
+++ application/views/page_attachment/view_ProjectFiles.php
@@ -20,11 +20,15 @@ if (!$file) {
 <?php if ($file->isPrivate()) { ?>
     <div class="private" title="<?php echo lang('private file') ?>"><span><?php echo lang('private file') ?></span></div>
 <?php } // if ?>
-    <div class="fileIcon"><img src="<?php echo $file->getTypeIconUrl() ?>" alt="<?php echo $file->getFilename() ?>" /></div>
+<?php if ($file->getFileType()->getIsImage()) { ?>
+  <img src="<?php echo $file->getDownloadUrl() ?>" style="max-height:100%; max-width:100%;">
+<?php } else { ?>
+    <!--- <div class="fileIcon"><img src="<?php echo $file->getTypeIconUrl() ?>" alt="<?php echo $file->getFilename() ?>" /></div> --->
     <div class="fileInfo">
       <span class="fileDescription"><?php echo $attachment->getText() ?>:</span>
       <span class="fileName"><a href="<?php echo $file->getDetailsUrl() ?>" title="<?php echo lang('view file details') ?>"><?php echo clean($file->getFilename()) ?></a></span>
     </div>
+<?php } // if ?>
     <div class="clear"></div>
   </div>
 <?php
