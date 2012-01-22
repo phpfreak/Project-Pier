9eb3e4b4f6377b6eca68cf6519d53553846dab07
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-22 12:15:04 +0100

    commit 193 new logic for page action buttons

--- application/layouts/pageoptions.php
+++ application/layouts/pageoptions.php
@@ -1,15 +1,17 @@
 <?php $page_actions = page_actions() ?>
 <?php if (is_array($page_actions)) { ?>
+<?php // latest logic: 1 element -> show, 2 elements -> show, 3 elements or more -> show first, rest in dropdown ?>
+<?php // end effect: max. 2 buttons on page, dropdown has min. 2 elements ?>
 <?php $page_actions_count = count($page_actions); ?>
               <ul id="page_menu">
                 <li><a href="<?php echo $page_actions[0]->getURL() ?>"><?php echo clean($page_actions[0]->getTitle()) ?></a></li>
-<?php if ($page_actions_count>1) { ?>
+<?php if ($page_actions_count==2) { ?>
                 <li><a href="<?php echo $page_actions[1]->getURL() ?>"><?php echo clean($page_actions[1]->getTitle()) ?></a></li>
 <?php } // if ?>
 <?php if ($page_actions_count>2) { ?>
                 <li class="align-right"><a href="#" onclick="return null;">...</a>
                   <ul>
-<?php for($i=2; $i<$page_actions_count; $i++) { ?>
+<?php for($i=1; $i<$page_actions_count; $i++) { ?>
                 <li><a href="<?php echo $page_actions[$i]->getURL() ?>"><?php echo clean($page_actions[$i]->getTitle()) ?></a></li>
 <?php } // for ?>
                   </ul>
