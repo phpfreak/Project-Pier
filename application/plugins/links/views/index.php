2a71cba30b087b4834f4e5a210d8f092d98c908f
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-22 19:33:58 +0100

    commit 203 move icon within block

--- application/plugins/links/views/index.php
+++ application/plugins/links/views/index.php
@@ -18,12 +18,12 @@
 <?php foreach ($links as $link) { ?>
 <?php $counter++; ?>
   <li class="item <?php echo $counter % 2 ? 'even' : 'odd' ?>">
+  <div class="block">
 <?php if ($show_icon) { ?>
   <div class="icon">
     <a rel="gallery" href="<?php echo $link->asUrl() . "?mime=text/html" ?>"><img src="<?php echo $link->getLogoURL(); ?>" alt="<?php echo clean($link->getTitle()) ?> logo"></a>
   </div>
 <?php } ?>
-  <div class="block">
   <div class="header"><a href="<?php echo $link->asUrl() ?>" title="<?php echo $link->getTitle() ?>" target="_blank"><?php echo $link->getTitle() ?></a></div>
   <div class="content"><div><?php echo clean($link->getDescription()) ?></div><?php if ($link->getCreatedBy() instanceof User) { ?><span><?php echo lang('created by') ?>:</span> <a href="<?php echo $link->getCreatedBy()->getCardUrl() ?>"><?php echo clean($link->getCreatedBy()->getDisplayName()) ?></a> <?php } ?><?php echo render_object_tags($link); ?>
 </div>
