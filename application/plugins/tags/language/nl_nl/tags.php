0fdeb98c86d0bcf800879c001e7e6ba061dd6905
Engelbert Mercelis <engelbertmercelis@gmail.com>
2012-01-25 15:52:11 +0100

    EB5: Tags plugin - nl
    Dutch translation of the tags plugin.
    Nederlandse vertaling van de tags plugin.

---
+++ application/plugins/tags/language/nl_nl/tags.php
@@ -0,0 +1,19 @@
+<?php
+ /**
+ * @author Engelbert Mercelis
+ * @licence Gnu Affero General Public License
+ */
+  return array(
+
+    'tag' => 'Tag',
+    'tags' => 'Tags',
+
+    'tags used on projects' => 'Tags gebruikt in dit project',
+    'number of tagged objects' => '%s object(en)',
+    'total objects tagged with' => 'Er zijn <b>%s</b> objecten voorzien van tag: <b>%s</b>, in dit project',
+    'no objects tagged with' => 'Er zijn geen objecten in dit project voorzien van tag: <b>%s</b>',
+    'tags widget description' => 'Lijst met door komma gescheiden tags (voorbeeld: "interface, javascript, publieke beta")',
+    
+  );
+
+?>
