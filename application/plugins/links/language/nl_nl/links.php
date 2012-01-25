422e5eec25d5b853289fe88558ca0c973a20010e
Engelbert Mercelis <engelbertmercelis@gmail.com>
2012-01-25 15:50:44 +0100

    EB3: Links plugin - nl
    Dutch translation of the links plugin.
    Nederlandse vertaling van de links plugin.

---
+++ application/plugins/links/language/nl_nl/links.php
@@ -0,0 +1,30 @@
+<?php
+ /**
+ * @author Engelbert Mercelis
+ * @licence Gnu Affero General Public License
+ */
+  return array(
+  
+    // Titles
+    'link' => 'Koppeling',
+    'links' => 'Koppelingen',
+    'my links' => 'Mijn koppelingen',
+    'project links' => 'Project koppelingen',
+    'add link' => 'Koppeling toevoegen',
+    'edit link' => 'Koppeling bewerken',
+    'url' => 'Koppeling URL',
+    'no links found' => 'Geen koppelingen gevonden voor dit project',
+    
+    'success add link' => 'Koppeling is toegevoegd',
+    'success edit link' => 'Koppeling is bijgewerkt',
+    'success delete link' => 'Koppeling is verwijderd',
+
+    'project link dnx' => 'Project koppeling niet gevonden',
+    
+    'log add projectlinks' => "Koppeling '%s' is aan het project toegevoegd",
+    'log edit projectlinks' => "Koppeling '%s' bijgewerkt",
+    'log delete projectlinks' => "Koppeling '%s' verwijderd",
+    
+  ); // array
+
+?>
