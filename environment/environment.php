5ee106879c6229034d1bcc45264e4d47e27ed4b0
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-26 21:13:55 +0100

    commit 216 session storage, http://www.projectpier.org/node/3105

--- environment/environment.php
+++ environment/environment.php
@@ -16,25 +16,38 @@
   if (!defined('ENVIRONMENT_PATH')) define('ENVIRONMENT_PATH', dirname(__FILE__));
   
   // Configure PHP
-  ini_set('short_open_tag', 'off');
+  @ini_set('short_open_tag', 'off');
 
   if (defined('DEBUG') && DEBUG) {
     //set_time_limit(120);
-    ini_set('display_errors', 1);
+    @ini_set('display_errors', 1);
     error_reporting(E_ALL);
   } else {
-    ini_set('display_errors', 0);
+    @ini_set('display_errors', 0);
   } // if
 
   //$timezone = config_option('timezone', 'GMT');
   $timezone = 'GMT';
   //$timezone = 'Europe/Sofia';
-  ini_set('date.timezone', $timezone );
+  @ini_set('date.timezone', $timezone );
   if (function_exists('date_default_timezone_set')) {
-    date_default_timezone_set($timezone);
+    @date_default_timezone_set($timezone);
   } else {
-    putenv("TZ=$timezone");
+    @putenv("TZ=$timezone");
   } // if
+
+  // http://www.php.net/manual/en/function.session-save-path.php#45733
+  // webmaster at gardenchemicals dot co dot uk 16-Sep-2004 07:59
+  // This is an absolute must if you have an important login on a shared server. Without it, other users of the server can do the following to bypass login:
+  // 
+  // * Visit login page, browse through cookies and grab the session id.
+  // * Create a PHP script on their account that grabs and sets session variables for a given session id.
+  // * Read and change any values for that session id (for example passwords or session keys), and therefore gain access to the protected area.
+  // 
+  // All users on web hosting should choose an dir below the HTTP directory struct, but within their user area to store the session files.
+  // 
+  ini_set('session.save_path',ROOT . '/tmp');
+  ini_set('session.gc_probability', 1);
 
   if (!ini_get('session.auto_start') || (strtolower(ini_get('session.auto_start')) == 'off')) {
     if(!session_id()) session_regenerate_id();
