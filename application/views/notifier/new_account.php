80cdea09af6053aac08a6b5db70724b520df8e9d
Reinier van Loon (sync server) <reinier.van.loon@gmail.com>
2012-01-26 21:13:54 +0100

    commit 214 displayname, http://www.projectpier.org/node/3141

--- application/views/notifier/new_account.php
+++ application/views/notifier/new_account.php
@@ -1,12 +1,7 @@
-<?php echo lang('hi john doe', $new_account->getDisplayName()) ?>,
-
-<?php echo lang('user created your account', $new_account->getCreatedByDisplayName()) ?>.
-
+<?php echo lang('hi john doe', $new_account->getContact()->getDisplayName()) ?>,
+<?php echo lang('user created your account', $new_account->getCreatedBy()->getContact()->getDisplayName()) ?>.
 <?php echo lang('visit and login', externalUrl(ROOT_URL)) ?>:
-
 <?php echo lang('username') ?>: <?php echo $new_account->getUsername() ?>
-
 <?php echo lang('password') ?>: <?php echo $raw_password ?>
-
 --
 <?php echo externalUrl(ROOT_URL) ?>
--- application/views/notifier/updated_account.php
+++ application/views/notifier/updated_account.php
@@ -1,6 +1,6 @@
-<?php echo lang('hi john doe', $updated_account->getDisplayName()) ?>,
+<?php echo lang('hi john doe', $updated_account->getContact()->getDisplayName()) ?>,
 
-<?php echo lang('user updated your account', $updated_account->getUpdatedByDisplayName()) ?>. <?php echo lang('visit and login', externalUrl(ROOT_URL)) ?>:
+<?php echo lang('user updated your account', $updated_account->getUpdatedBy()->getContact()->getDisplayName()) ?>. <?php echo lang('visit and login', externalUrl(ROOT_URL)) ?>:
 <?php echo lang('username') ?>: <?php echo $updated_account->getUsername() ?> 
 <?php if (trim($raw_password) != '') { ?>
 <?php echo lang('password') ?>: <?php echo $raw_password ?>
