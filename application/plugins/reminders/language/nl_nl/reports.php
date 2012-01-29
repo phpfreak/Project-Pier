3ad2783915170ad5118b5999929aafd9399ec406
Engelbert Mercelis <engelbertmercelis@gmail.com>
2012-01-29 18:09:34 +0100

    EB9: Reminders plugin - nl …
    Dutch translation of the Reminders plugin.
    Nederlandse vertaling van de Herinneringen plugin.

---
+++ application/plugins/reminders/language/nl_nl/reports.php
@@ -0,0 +1,54 @@
+<?php
+ /**
+ * @author Engelbert Mercelis
+ * @licence Gnu Affero General Public License
+ */
+  return array(
+
+	'reminder options' => 'Herinneringen Opties',
+	'report options' => 'Rapport Opties',
+	
+	'reminder enable' => 'Herinneringen Activeren',
+	'report enable' => 'Wekelijkse Rapporten Activeren',
+	'summarize by' => 'Samenvatten door',
+	
+	'per project' => 'Per Project',
+	'summarized' => 'Samengevat',
+	
+	'show tasks in future' => 'Toon Taken <i>X</i> Dagen In De Toekomst',
+	
+	'include whom question' => 'Voor Projecten / Mijlpalen toegewezen aan mij, voeg ook taken toe',
+	'for me' => 'Voor Mezelf',
+	'for all' => 'Voor Iedereen',
+	
+	'days to send' => 'Dagen waarop E-mail verstuurdt dient te worden',
+	'day to send' => 'Dag waarop E-mail verstuurdt dient te worden',
+	'include activity log' => 'Voeg Activiteits Log Toe',
+	
+	'Monday' => 'Maandag',
+	'Tuesday' => 'Dinsdag',
+	'Wednesday' => 'Woensdag',
+	'Thursday' => 'Donderdag',
+	'Friday' => 'Vrijdag',
+	'Saturday' => 'Zaterdag',
+	'Sunday' => 'Zondag',
+	
+	'update prefs' => 'Bewerk Voorkeuren',
+	'prefs updated' => 'Voorkeuren Bijwerken Succesvol',
+	
+	'project' => 'Project',
+	'milestone' => 'Mijlpaal',
+	'task' => 'Taak',
+	'all' => 'Alles in één',
+	
+	'is today' => 'is tegen vandaag!',
+	'is late' => 'is %s dagen te laat!',
+	'is future' => 'is te laat over %s dagen.',
+	
+	'action' => 'Actie'
+  );
+
+?> 
+
+
+
