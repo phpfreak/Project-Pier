2827cff9fda70982d4e672ee7d895dfb76fa6a87
egofer <emilio.gomez.fdez@gmail.com>
2014-08-17 17:52:05 +0200

    Update site_interface.php to Spanish

--- language/es_es/site_interface.php
+++ language/es_es/site_interface.php
@@ -8,7 +8,7 @@
   
   // Return langs
   return array(
-    'new version available' => 'Hay una nueva versión disponible de ProjectPier. <a href="%s">Ver detalles</a>.',
+    'new version available' => 'Existe una nueva versión disponible de ProjectPier. <a href="%s">Ver detalles</a>.',
     
     'my tasks' => 'Mis tareas',
     'welcome back' => 'Hola <strong>%s</strong>',
@@ -23,36 +23,37 @@
     
     'my projects' => 'Mis proyectos',
     'my projects archive desc' => 'Lista de proyectos terminados (archivados). Todas las acciones en estos proyectos están deshabilitadas pero aun así puedes visualizarlo.',
+    'my files' => 'Mis archivos',
     
-    'company online' => 'Compañía en línea',
+    'company online' => 'Empresa en línea',
     
     'enable javascript' => 'Habilita JavaScript en tu navegador para utilizar esta característica',
     
     'user password generate' => 'Generar contraseña al azar',
     'user password specify' => 'Especificar contraseña',
     'is administrator' => 'Administrador',
-    'is auto assign' => '¿Auto asignar a nuevos proyectos?',
-    'auto assign' => 'Auto-asignar',
-    'administrator update profile notice' => 'Opciones de administrador (disponibles solo a administradores!)',
+    'is auto assign' => '¿Autoasignar a nuevos proyectos?',
+    'auto assign' => 'Autoasignar',
+    'administrator update profile notice' => 'Opciones de administrador (disponibles solo para administradores!)',
     
     'project completed on by' => 'Completado el %s por %s',
 
-    'use LDAP' => 'Autenticar utilizando LDAP',
+    'use LDAP' => 'Autentificarse mediante LDAP',
     'LDAP' => 'Autenticación LDAP',
     
     'im service' => 'Servicio',
     'primary im service' => 'Mensajero instantaneo principal',
-    'primary im description' => 'Todas las direcciones de mensajería instantanea que introduzcas se mostraran en tu página de perfil. Solo la dirección primaria de mensajería se mostrará en otras páginas (como la página de personas del proyecto).',
+    'primary im description' => 'Todas las direcciones de mensajería instantanea que introduzca se mostrarán en su página de perfil. Solo la dirección primaria de mensajería se mostrará en otras páginas (como la página de personas del proyecto).',
     'contact online' => 'Contacto en línea',
     'contact offline' => 'Contacto desconectado',
     
     'avatar' => 'Avatar',
     'current avatar' => 'Avatar actual',
-    'current logo' => 'Logo actual',
+    'current logo' => 'Logotipo actual',
     'new avatar' => 'Nuevo avatar',
-    'new logo' => 'Nuevo logo',
-    'new avatar notice' => 'El avatar actual será eliminado y reemplazado con el nuevo!',
-    'new logo notice' => 'El logo actual será eliminado y reemplazado con el nuevo!',
+    'new logo' => 'Nuevo logotipo',
+    'new avatar notice' => '¡El avatar actual será eliminado y reemplazado con el nuevo!',
+    'new logo notice' => '¡El logotipo actual será eliminado y reemplazado con el nuevo!',
     
     'days late' => '%s días atrasado',
     'day late' => '%s día atrasado',
@@ -72,17 +73,15 @@
     
     // Complete installation
     'complete installation' => 'Completar la instalación',
-    'complete installation desc' => 'Este es el paso final en el proceso de instalación que te permitirá crear una cuenta de administrador e introducir una breve descripción acerca de tu compañía',
+    'complete installation desc' => 'Este es el paso final en el proceso de instalación que le permitirá crear una cuenta de administrador e introducir una breve descripción sobre de su empresa',
     
     // Administration
-    'welcome to administration' => 'Benvenido',
-    'welcome to administration info' => 'Bienvenido al panel de administración. Utilizando esta herramienta puedes administrar la información de la compañía, miembros, clientes y proyectos en los que estás involucrado.',
+    'welcome to administration' => 'Bienvenido',
+    'welcome to administration info' => 'Bienvenido al panel de administración. Utilizando esta herramienta puede administrar la información de la empresa, miembros, clientes y proyectos en los que está involucrado.',
     
     'send new account notification' => '¿Enviar correo de notificación?',
-    'send new account notification desc' => 'Si seleccionas "Si" el usuario recibirá un correo con un mensaje de bienvenida y datos de inicio de sesión',
-    
-    'installation' => 'Instalación',
-    'Select language' => 'Selecciona idioma',
+    'send new account notification desc' => 'Si seleccionas "Sí" el usuario recibirá un correo con un mensaje de bienvenida y los datos de inicio de sesión',
+
     // Tools
     'administration tools' => 'Herramientas',
     
@@ -95,21 +94,14 @@
     'massmailer recipients' => 'Destinatarios',
     
     // Dashboard
-    'welcome to new account' => 'Benivenido a tu cuenta recien creada',
-    'welcome to new account info' => '%s, bienvenido a tu nueva cuenta. De ahora en adelante estará disponible en %s (puedes marcar como favorito este enláce). Puedes comenzar en breve siguiendo estos sencillos pasos:',
-    'new account step1' => 'Paso 1: Actualiza la información de tu compañía',
-    'new account step1 info' => '<a href="%s">Establece los detalles de tu compañía</a> como teléfono, fax, domicilio, correo electrónico, sitio web, etc.',
-    'new account step2' => 'Paso 2: Agrega miembros de tu equipo',
-    'new account step2 info' => 'Puedes <a href="%s">crear cuentas de usuario</a> para todos los miembros de tu equipo. Cada miembro obtendrá un nombre de usuario y una contraseña que pueden utilizar para acceder al sistema',
-    'new account step3' => 'Paso 3: Agregar compañías de clientes y sus miembros',
-    'new account step3 info' => 'Ahora es tiempo de <a href="%s">definir compañías de clientes</a> (ilimitadas). Cuando hayas terminado puedes agregar sus miembros o dejar que los lideres de esos equipos lo hagan. Miembros de clientes son similares a los miembros de tu compañía excepto que ellos tienen acceso limitado al contenido y funciones (puedes establecer que pueden hacer por proyecto y por miembro)',
-    'new account step4' => 'Paso 4: Comienza un proyecto',
-    'new account step4 info' => 'Definir un <a href="%s">nuevo proyecto</a> es muy fácil: establece un nombre y una descripción (opcional) y da click. Después, puedes establecer los permisos para los miembros de tu equipo y tus clientes.',
-    'add welcome task text' => 'Hola %s, bienvenido a tu nueva cuenta. Por favor actualiza tu perfil dentro de los siguientes 7 días aquí %s.',
-    'add welcome task' => 'Agregar una tarea al proyecto de bienvenida',
-    'add welcome task desc' => 'Una tarea es agregada a la lista de tareas del proyecto de bienvenida con un texto de introducción e instrucciones para actualizar su perfil.',
+    'welcome to new account' => 'Bienvenido a su cuenta recien creada',
+    'welcome to new account info' => '%s, bienvenido a su nueva cuenta. De ahora en adelante estará disponible en %s (puede marcar como favorito este enlace). Puede comenzar en breve siguiendo estos sencillos pasos:',
+    'add welcome task text' => 'Hola %s, vbieveniedo a su nueva cuenta. Por favor actualice su erfil dentro de los 7 días siguientes desde aquí %s.',
+    'add welcome task' => 'Añadir una tarea al proyecto Welcome',
+    'add welcome task desc' => 'Una tarea fue agregada a la lista de tareas del proyecto Welcome con un texto de introducción e instrucciones para actualizar su perfil.',
     
     // Application log
+    'application log events my projects' => 'Eventos para mi proyecto',
     'application log type column name' => 'Tipo',
     'application log date column name' => 'Fecha',
     'application log details column name' => 'Detalles',
@@ -123,7 +115,7 @@
     'recent project activities feed' => 'Actividades recientes en el proyecto %s',
     
     // Update company permissions
-    'update company permissions hint' => 'Ver que proyectos puede acceder esta empresa. Necesitarás especificar permisos de acceso para miembros de compañías que quieran acceder y administrar los proyectos seleccionados (puedes hacer a través de la página de Personas del proyecto o por medio de sus perfiles de usuario).',
+    'update company permissions hint' => 'Ver que proyectos puede acceder esta empresa. Necesitará especificar permisos de acceso para miembros de compañías que quieran acceder y administrar los proyectos seleccionados (puede hacer esto a través de la página de Personas del proyecto o por medio de sus perfiles de usuario).',
     
     'footer copy with homepage' => '&copy; %s por <a href="%s">%s</a>. Todos los derechos reservados.',
     'footer copy without homepage' => '&copy; %s por %s. Todos los derechos reservados',
