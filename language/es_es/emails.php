57b50f16021a388e7724887c9ddf4dd23f3d3bbb
egofer <emilio.gomez.fdez@gmail.com>
2014-08-18 10:36:49 +0200

    Update emails.php to Spanish

--- language/es_es/emails.php
+++ language/es_es/emails.php
@@ -5,30 +5,35 @@
     // Titles
     'new message' => 'Nuevo mensaje',
     'new comment' => 'Nuevo comentario',
-    'your account created' => 'Tu cuenta ha sido creada',
-    'your password' => 'Tu contraseña',
-    'milestone assigned to you' => 'Un milestone te ha sido asignado',
-    'task assigned to you' => 'Una tarea te ha sido asignada',
+    'your account created' => 'Su cuenta ha sido creada',
+    'your account updated' => 'Su cuenta ha sido actualizada',
+    'your password' => 'Su contraseña',
+    'milestone assigned to you' => 'Un hito le ha sido asignado',
+    'task assigned to you' => 'Una tarea la ha sido asignada',
     
     // Interface
     'hi john doe' => 'Hola %s',
     
-    'user password reset' => 'Tu contraseña ha sido reestablecida. Tu nueva contraseña es \'%s\'.',
+    'user password reset' => 'Su contraseña ha sido reestablecida. Su nueva clave es \'%s\'.',
+    'password unchanged' => 'Su contraseña se mantiene sin cambios.',
     
-    'do not reply warning' => 'ESTE ES UN MENSAJE GENERADO AUTOMÁTICAMENTE. POR FAVOR NO RESPONDAS ESTE CORREO.',
-    'new message posted' => 'Un nuevo mensaje "%s" ha sido escrito en el proyecto "%s"',
-    'view new message' => 'Ver mensaje',
+    'do not reply warning' => 'ESTE ES UN MENSAJE GENERADO AUTOMÁTICAMENTE. POR FAVOR NO RESPONDA ESTE CORREO.',
+    'new message posted' => 'Un nuevo mensaje "%s" ha sido publicado en el proyecto "%s"',
+    'new file posted' => 'Un nuevo "%s" ha sido publicado en el proyecto "%s"',
+    'view new message' => 'Ver este mensaje',
+    'view new file' => 'Ver este archivo',
     
-    'new comment posted' => 'Un nuevo comentario ha sido escrito en el proyecto "%s"',
-    'view new comment' => 'Ver comentario',
+    'new comment posted' => 'Un nuevo comentario ha sido publicado en el proyecto "%s"',
+    'view new comment' => 'Ver este comentario',
     
-    'user created your account' => '%s ha creado una nueva cuenta para ti',
-    'visit and login' => 'Puedes ir a %s e iniciar sesión con ella',
+    'user created your account' => '%s ha creado una nueva cuenta para usted',
+    'user updated your account' => '%s actualizó su cuenta',
+    'visit and login' => 'Puede ir a %s e iniciar sesión con ella',
     
-    'milestone assigned' => 'El milestone "%s" te ha sido asignado',
-    'view assigned milestones' => 'Ver milestone',
+    'milestone assigned' => 'El hito "%s" le ha sido asignado',
+    'view assigned milestones' => 'Ver hito',
 
-    'task assigned' => 'La tarea "%s" te ha sido asignada',
+    'task assigned' => 'La tarea "%s" le ha sido asignada',
     
   ); // array
 
